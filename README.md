d2007fc8056194b23fed8123ec3cc6e1ce77563d
macfluffy <shadowimitation@hotmail.com>
2025-09-20 10:12:19 +1000

    feat : Creating the initialisation functions for the flask app

---
+++ README.md
@@ -0,0 +1,4 @@
+## Steps to run the API
+- Create a database, user with Read/Write privileges to the database
+- Create an .env file
+- Define the DATABASE_URI value
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from init import db
+import os
+
+# load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    print("Flask server started.")
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI") # requires load_dotenv()
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+    db.init_app(app)
+    return app
