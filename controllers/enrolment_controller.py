e7fc92d99dfc1dcc70c8e1cd68f2327bcd181e09
macfluffy <shadowimitation@hotmail.com>
2025-09-29 20:56:00 +1000

    feat (Enrolment) : Read all route works with course and student filters

--- controllers/enrolment_controller.py
+++ controllers/enrolment_controller.py
@@ -12,7 +12,17 @@ def error_empty_table():
 # Read all
 @enrolments_bp.route("/")
 def get_enrolments():
+    course_id = request.args.get("course_id", type = int)
+    student_id = request.args.get("student_id", type = int)
     stmt = db.select(Enrolment)
+    
+    if course_id:
+        stmt = stmt.where(Enrolment.course_id == course_id)
+    
+    if student_id:
+        stmt = stmt.where(Enrolment.student_id == student_id)
+        # stmt = stmt.filter_by(Student.student_id == student_id)
+
     enrolments_list = db.session.scalars(stmt)
     queryData = enrolments_schema.dump(enrolments_list)
     if queryData:
