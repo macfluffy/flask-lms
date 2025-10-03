58eb71bcc1ba2f3d03218a2b0e7ed24159ad2955
macfluffy <shadowimitation@hotmail.com>
2025-10-03 12:59:54 +1000

    fix : Documenting purpose of init.py

--- init.py
+++ init.py
@@ -1,3 +1,5 @@
+# External libraries
 from flask_sqlalchemy import SQLAlchemy
 
+# Initialise by creating a single instance of the Object Relational Mapper that we can refer to throughout the code
 db = SQLAlchemy()
