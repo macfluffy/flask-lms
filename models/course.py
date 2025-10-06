01f53b2e161126c0211a437aabb8296f7207dd98
macfluffy <shadowimitation@hotmail.com>
2025-10-06 17:39:15 +1100

    docs (Course) : Adding scope context to the course model.

--- models/course.py
+++ models/course.py
@@ -1,13 +1,19 @@
 """
-The course table template contains the names of the course, the duration
-the course will run til completion, and the teacher who will be teaching
-it. 
+This file defines the model for the 'course' table and it's relationships with
+'teachers' and the 'enrolments' models. 
 """
 
 # Local imports
 from init import db
 
 class Course(db.Model):
+    """
+    The course table template contains the names of the course, the duration
+    the course will run til completion, and the teacher who will be teaching
+    it. 
+    """
+
+    # Name of the table and what is referenced by Flask-SQLAlchemy methods
     __tablename__ = "courses"
 
     # Table columns
@@ -15,9 +21,11 @@ class Course(db.Model):
     name = db.Column(db.String(100), nullable = False, unique = True)
     duration = db.Column(db.Float, nullable = False)
     
-    # Foreign Key: Teacher
+    # Foreign Key: Teacher ID is the common link between 
+    # the course and teacher tables
     teacher_id = db.Column(db.Integer, db.ForeignKey("teachers.teacher_id"))
     
-    # Define the relationship between teachers teaching courses, and student course enrolments
+    # Define the relationship between teachers teaching courses, and 
+    # student course enrolments
     teacher = db.relationship("Teacher", back_populates = "courses")
     enrolments = db.relationship("Enrolment", back_populates = "course")
